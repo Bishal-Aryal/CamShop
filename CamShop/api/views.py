ee9571c51cb22649ae96fc84927a5a66db92d53a
Bishal aryal <bishalaryal1225@gmail.com>
2024-04-23 01:08:39 +0545

    another

--- CamShop/api/views.py
+++ CamShop/api/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework import status
+from django.contrib.auth.hashers import make_password
 from .models import Product
-from .serializers import ProductSerializer,UserSerializer
-from .products import products
+from .serializers import ProductSerializer,UserSerializer,UserSerializerWithToken
+
 
 # Create your views here.
 @api_view(['GET'])
@@ -25,24 +28,17 @@ def getProduct(request,pk):
     return Response(serializer.data)
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-
-        # Add custom claims
-        token['username'] = user.username
-        token['message'] = 'hello'
-        # ...
-
-        return token
-    
-class MyTokenObtainPariView(TokenObtainPairView):
+   
     def validate(self,attrs):
         data=super().validate(attrs)
-        data['username']=self.user.username
-        data['email']=self.user.email
-        
+        serializer = UserSerializerWithToken(self.user).data
+        for k,v in serializer.items():
+            data[k]=v
+           
         return data
+    
+class MyTokenObtainPariView(TokenObtainPairView):
+    serialzer_class = MyTokenObtainPairSerializer
     
 @api_view(['GET'])
 def  getUserProfiles(request):
@@ -50,3 +46,45 @@ def  getUserProfiles(request):
     serializer=UserSerializer(user,many=False)
     return Response(serializer.data)
         
+@api_view(['GET'])
+def  getUsers(request):
+    user=User.objects.all()
+    serializer=UserSerializer(user,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def  getUserProfiles(request):
+    user=request.user
+    serializer=UserSerializer(user,many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def  getUsers(request):
+    user=User.objects.all()
+    serializer=UserSerializer(user,many=True)
+    return Response(serializer.data)
+
+
+# register the new users
+
+@api_view(['POST'])
+def registerUser(request):
+    data=request.data
+    print(data)
+    try:
+
+        user=User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer=UserSerializerWithToken(user,many=False)
+        return Response(serializer.data)
+    except:
+        message={'details':'USER WITH THIS EMAIL ALREADY EXIST'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
